70c8765b7206269e79ed12f53add6125ec171639
Anton Koshkin <akoshkin@devexperts.com>
2019-05-03 20:46:14 +0300

    fix: oauth-flows-object fix

--- src/validate/open-api-3/oauth-flows-object/index.ts
+++ src/validate/open-api-3/oauth-flows-object/index.ts
@@ -1,21 +1,38 @@
 import { Option } from 'fp-ts/lib/Option';
-import { OAuthFlowObject, oauthFlowObjectIO } from '../oauth-flow-object';
-import { type } from 'io-ts';
+import { intersection, record, string, type } from 'io-ts';
 import { createOptionFromNullable } from 'io-ts-types';
+
+// https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#oauthFlowObject
+type AuthorizationURL = { authorizationUrl: string };
+const authorizationURLIO = type({ authorizationUrl: string });
+
+type TokenURL = { tokenUrl: string };
+const tokenURLIO = type({ tokenUrl: string });
+
+type RefreshURLAndScopes = {
+	refreshUrl: Option<string>;
+	scopes: Record<string, string>;
+};
+const refreshURLAndScopesIO = type({
+	refreshUrl: createOptionFromNullable(string),
+	scopes: record(string, string),
+});
 
 // https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.0.md#oauthFlowsObject
 export type OAuthFlowsObject = {
-	implicit: Option<OAuthFlowObject>;
-	password: Option<OAuthFlowObject>;
-	clientCredentials: Option<OAuthFlowObject>;
-	authorizationCode: Option<OAuthFlowObject>;
+	authorizationCode: Option<RefreshURLAndScopes & TokenURL & AuthorizationURL>;
+	clientCredentials: Option<RefreshURLAndScopes & TokenURL>;
+	implicit: Option<RefreshURLAndScopes & AuthorizationURL>;
+	password: Option<RefreshURLAndScopes & TokenURL>;
 };
 export const oauthFlowsObjectIO = type(
 	{
-		implicit: createOptionFromNullable(oauthFlowObjectIO),
-		password: createOptionFromNullable(oauthFlowObjectIO),
-		clientCredentials: createOptionFromNullable(oauthFlowObjectIO),
-		authorizationCode: createOptionFromNullable(oauthFlowObjectIO),
+		authorizationCode: createOptionFromNullable(
+			intersection([refreshURLAndScopesIO, tokenURLIO, authorizationURLIO]),
+		),
+		clientCredentials: createOptionFromNullable(intersection([refreshURLAndScopesIO, tokenURLIO])),
+		implicit: createOptionFromNullable(intersection([refreshURLAndScopesIO, authorizationURLIO])),
+		password: createOptionFromNullable(intersection([refreshURLAndScopesIO, tokenURLIO])),
 	},
 	'OAuthFlowsObject',
 );
